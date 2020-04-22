876d591dfd61411c4002c3ea928d27201ec1ec43
Christopher Dang <christhd@uci.edu>
2020-04-22 10:16:34 -0700

    Its a hand that kinda works

---
+++ 168-Project/Assets/Hand_Assets/finger_move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finger_move : MonoBehaviour
+{
+    public string key;
+    public GameObject Finger_base;
+    public GameObject Finger_1;
+    public GameObject Finger_2;
+    public GameObject Finger_3;
+    public float moveSpeed = 1f;
+    public float rotationAngle = 90f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key))
+        {
+            Finger_3.transform.Rotate(new Vector3(rotationAngle, 0, 0) * Time.deltaTime * moveSpeed);
+        }
+    }
+}
