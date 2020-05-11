fe72f4d6a143f69a98524e7499ea683abfea43da
Ian Maynard <relreothegreat@gmail.com>
2020-05-11 14:19:02 -0700

    Sink Handles
    Added sink handles that rotate on mouse drag. Currently has unlimted range of ration, which needs to be fixed.

---
+++ 168-Project/Assets/Scripts/HandleRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleRotation : MonoBehaviour
+{
+    float rotSpeed = 50;
+    // Start is called before the first frame update
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        Vector3 rotationPoint = transform.position;
+        transform.Rotate(Vector3.up, -rotX);
+    }
+}
