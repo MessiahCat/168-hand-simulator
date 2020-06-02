0c96f94aa4a8fbc31db8d3fb446fa3fe4ae20eca
Christopher Dang <christhd@uci.edu>
2020-06-01 19:32:04 -0700

    Added a touch script to the left hand so it knows if it has been touched.

---
+++ 168-Project/Assets/Hand_Assets/Touched_Me.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touched_Me : MonoBehaviour
+{
+    public bool touched = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            touched = true;
+        }
+    }
+}
