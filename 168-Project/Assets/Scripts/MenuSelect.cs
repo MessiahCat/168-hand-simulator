8d1428b1c5cde437ab97fa89bcdee46a2d171fee
Christopher Dang <christhd@uci.edu>
2020-06-08 10:24:29 -0700

    Very basic menu added

---
+++ 168-Project/Assets/Scripts/MenuSelect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuSelect : MonoBehaviour
+{
+    private void Start()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void select(string LevelName)
+    {
+        SceneManager.LoadScene(LevelName, LoadSceneMode.Single);
+    }
+}
